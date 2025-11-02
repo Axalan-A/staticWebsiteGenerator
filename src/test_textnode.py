91761cc9221dc29aa64391ab5c94b2bdb99cfb4b
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2025-11-02 00:56:25 -0500

    Implement unit tests

---
+++ src/test_textnode.py
@@ -0,0 +1,32 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        self.assertEqual(node, node2)
+
+    def test_noteq(self):
+        node = TextNode("This has a url", TextType.LINK, url="tester.com")
+        node2 = TextNode("This is a url too! But different text", TextType.LINK, url="tester.com")
+        self.assertNotEqual(node, node2)
+     
+    def test_blankURLeq(self):
+        node = TextNode("This is the same url", TextType.LINK)
+        node2 = TextNode("This is the same url", TextType.LINK)
+        self.assertEqual(node, node2)
+
+    def test_sameTextDiffLink(self):
+        node = TextNode("This url goes here!", TextType.LINK, url="here.com")
+        node2 = TextNode("This url goes here!", TextType.LINK, url="nothere.com")
+        self.assertNotEqual(node, node2)
+
+    def test_differentType(self):
+        node = TextNode("Hello! I'm text!", TextType.BOLD_TEXT)
+        node2 = TextNode("Hello! I'm text!", TextType.ITALIC_TEXT)
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
