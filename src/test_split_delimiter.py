a204f1b873bf5e845b566ec6865e66440b18e19b
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2025-11-04 17:30:09 -0600

    Implement delimiter tests

---
+++ src/test_split_delimiter.py
@@ -0,0 +1,9 @@
+import unittest
+from split_delimiter import split_nodes_delimiter
+from textnode import TextNode, TextType
+
+
+class TestDelimiterSplit(unittest.TestCase):
+    def test_codeblock(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
