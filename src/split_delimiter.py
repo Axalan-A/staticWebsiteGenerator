77f22e2e1e8206d2154250bc1d2367c653ed0310
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2025-11-04 17:37:02 -0600

    Fixed edge case error

--- src/split_delimiter.py
+++ src/split_delimiter.py
@@ -7,12 +7,17 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         if node.text_type is not TextType.TEXT:
             new_nodes.append(node)
         else:
-            split_text = node.value.split(delimiter)
+            split_text = node.text.split(delimiter)
+            if len(split_text) % 2 == 0:
+                raise ValueError("invalid format. delimitter not closed")
             # Oh wait. The delimited point will always be an even index
-            for block in range(0, len(split_text)):
-                if block % 2 == 0:
+            for block in range(len(split_text)):
+                if split_text[block].text == "":
+                    continue
+                if block % 2 == 1:
                     new_nodes.append(TextNode(split_text[block], text_type=text_type))
                 else:
                     new_nodes.append(
                         TextNode(split_text[block], text_type=TextType.TEXT)
                     )
+    return new_nodes
--- src/test_split_delimiter.py
+++ src/test_split_delimiter.py
@@ -6,4 +6,10 @@ from textnode import TextNode, TextType
 class TestDelimiterSplit(unittest.TestCase):
     def test_codeblock(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE_TEXT)
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE_TEXT),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(expected, new_nodes)
