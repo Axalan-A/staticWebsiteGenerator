236f59bc0872ff35c56629bc181b15e1681addf4
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2025-11-03 19:49:12 -0600

    Implement tests for ParentNode

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -16,3 +16,16 @@ class TestHTMLNode(unittest.TestCase):
             parent_node.to_html(),
             "<div><span><b>grandchild</b></span></div>",
     )
+
+    def test_no_children(self):
+        parent_node = ParentNode("b", children=None)
+        self.assertRaises(ValueError, lambda: parent_node.to_html())
+
+    def test_multiple_children(self):
+        child1 = LeafNode("p", "Hello! First paragraph")
+        child2 = LeafNode("a", "This one's a link", {"href": "link.com"})
+        child3 = LeafNode("p", "And then a closing paragraph")
+        parent_node = ParentNode("div", children=[child1, child2, child3])
+        expected = "<div><p>Hello! First paragraph</p><a href=\"link.com\">This one's a link</a><p>And then a closing paragraph</p></div>"
+        self.assertEqual(expected, parent_node.to_html())
+
