d273b775fab4b28791ae7489436ddd5950cb9608
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2025-11-06 17:48:23 -0600

    Implement text to textnodes tests

---
+++ src/test_texttonode.py
@@ -0,0 +1,25 @@
+import unittest
+from textnode import TextNode, TextType
+from text_to_textnodes import text_to_textnodes
+
+
+class TestTextToNode(unittest.TestCase):
+    def test_all_types(self):
+        output = text_to_textnodes(
+            "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        )
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode(
+                "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+            ),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertEqual(output, expected)
