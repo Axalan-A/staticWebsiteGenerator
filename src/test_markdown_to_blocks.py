78d2035da9db6e18178f401b5f520ab8df18d07c
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2025-11-07 00:23:43 -0600

    Implement final markdown to block tests

--- src/test_markdown_to_blocks.py
+++ src/test_markdown_to_blocks.py
@@ -22,3 +22,39 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_markdown_excessive_newlines(self):
+        md = """
+Hello! This one has so much lines!
+
+
+
+
+
+I wonder
+if it works?
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "Hello! This one has so much lines!",
+                "I wonder\nif it works?",
+            ],
+        )
+
+    def test_markdown_excessive_spaces(self):
+        md = """
+This one has plenty of extra spaces          
+
+
+        Any errors here?        
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This one has plenty of extra spaces",
+                "Any errors here?",
+            ],
+        )
