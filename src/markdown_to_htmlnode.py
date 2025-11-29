cb41ed588c4bc0f2be2175d9086fc742b3e87885
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2025-11-28 18:29:40 -0600

    refactor: return markdown_to_html to blank slate

--- src/markdown_to_htmlnode.py
+++ src/markdown_to_htmlnode.py
@@ -4,12 +4,10 @@ from blockType import BlockType, block_to_block_type
 from text_to_textnodes import text_to_textnodes
 from textnode import TextNode, TextType
 
-def text_to_children(text, list=False):
+def text_to_children(text):
     text_nodes = text_to_textnodes(text.replace("\n", " ").strip())
     leaves = []
     for node in text_nodes:
-        if list:
-            node.tag = "li"
         leaves.append(node.text_node_to_html_node())
     return leaves
 
@@ -17,30 +15,19 @@ def text_to_children(text, list=False):
 def block_to_node(block):
     block_type = block_to_block_type(block)
     if block_type == BlockType.HEADING:
-        sections = block.split(" ")
-        tag = f"h{len(sections[0])}"
+        pass
     elif block_type == BlockType.QUOTE:
-        tag = "blockquote"
+        pass
     elif block_type == BlockType.UNORDERED:
-        tag = "ul"
-        return ParentNode(tag=tag, children=text_to_children(block, list=True))
+        pass
     elif block_type == BlockType.ORDERED:
-        tag = "ol"
-        return ParentNode(tag=tag, children=text_to_children(block, list=True))
+        pass
     elif block_type == BlockType.CODE:
-        tag = "code"
-        return ParentNode(
-            tag="pre",
-            children=ParentNode(
-                tag=tag, children=TextNode(text=block, text_type=TextType.TEXT)
-            ),
-        )
+        pass
     elif block_type == BlockType.PARAGRAPH:
-        tag = "p"
+        pass
     else:
         raise ValueError("Invalid Type")
-
-    return ParentNode(tag=tag, children=text_to_children(block))
 
 
 def markdown_to_html_node(markdown):
