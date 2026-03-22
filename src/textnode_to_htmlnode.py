c5e07ca9aae0e28e874ace14ca0028502513ea06
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2026-03-22 01:31:34 -0500

    feat: Implement text node to leaf node conversion

---
+++ src/textnode_to_htmlnode.py
@@ -0,0 +1,21 @@
+from leafnode import LeafNode
+from htmlnode import HTMLNode
+from textnode import TextNode, TextType
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    if text_node.text_type not in TextType:
+        raise ValueError("Invalid text type")
+    else:
+        tag = ""
+        if text_node.text_type == TextType.TEXT:
+            return LeafNode(tag=None, value = text_node.text)
+        elif text_node.text_type == TextType.BOLD:
+            return LeafNode(tag = "b", value = text_node.text)
+        elif text_node.text_type == TextType.ITALIC:
+            return LeafNode(tag = "i", value = text_node.text)
+        elif text_node.text_type == TextType.CODE:
+            return LeafNode(tag = "code", value = text_node.text)
+        elif text_node.text_type == TextType.LINK:
+            return LeafNode(tag = "a", value = text_node.text, props = {"href": text_node.url})
+        else:
+            return LeafNode(tag = "img", value = None, props = {"src": text_node.url, "alt": text_node.text})
