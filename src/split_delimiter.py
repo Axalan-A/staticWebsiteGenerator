e593651a42537a53aab408751dc8c489098c4d8d
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2026-03-28 19:28:44 -0500

    feat: implement delimiter splitter

---
+++ src/split_delimiter.py
@@ -0,0 +1,24 @@
+from textnode import TextNode, TextType
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type: TextType):
+    new_nodes = []
+    for node in old_nodes:
+        text = node.text
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        if text.count(delimiter) % 2 != 0:
+            raise ValueError("Unclosed delimiter")
+        splits = text.split(delimiter)
+        for index in range(len(splits)):
+            if splits[index] == "":
+                continue
+            if index % 2 == 1:
+                new_node = TextNode(splits[index], text_type)
+
+            else:
+               new_node = TextNode(splits[index], TextType.TEXT)
+
+            new_nodes.append(new_node)
+        return new_nodes
