f4169010a048e6355deefea5dd208ddddd3f3a72
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2026-03-30 00:43:10 -0500

    feat: more tests added to markdown extraction

--- src/test_extract_properties.py
+++ src/test_extract_properties.py
@@ -7,3 +7,26 @@ class ExtractionTests(unittest.TestCase):
             "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
         )
         self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_link(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("link", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_wrong_func_link(self):
+        matches = extract_markdown_images(
+            "This is text with a [link](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([], matches)
+
+    def test_wrong_func_image(self):
+        matches = extract_markdown_links(
+            "This is text with a ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([], matches)
+    def test_brackets_in_link(self):
+        matches = extract_markdown_links(
+            "This is text with a [A link! with [these]](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("A link! with [these]", "https://i.imgur.com/zjjcJKZ.png")], matches)
