c3cb2808cb174db7f266d186a237de8d66067381
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2026-04-19 13:24:10 -0500

    feat: implement function to copy static/ to public/

--- src/main.py
+++ src/main.py
@@ -1,4 +1,29 @@
-from textnode import TextNode
+# The goal here is to take everything within the static folder into the public folder
+# The steps are:
+# Delete the contents of the public folder
+# Copy all files, subdirectiories and nested files from static into public
+# Possibly log all actions for debugging purposes
 
-test_node = TextNode("Hello World!", "text", url=None)
-print(test_node)
+import os
+import shutil
+
+# Loop through all files/folders in public:
+shutil.rmtree("public")
+os.mkdir("public")
+
+def copy_site_files(path, target):
+    # Now, look through static and begin copying
+    static_files = os.listdir(path)
+    for file in static_files:
+        current_path = os.path.join(path, file)
+        destination_path = os.path.join(target, file)
+        if not os.path.isfile(current_path):
+            print(f"Creating directory {destination_path}")
+            new_target = os.path.join(target, file)
+            os.mkdir(destination_path)
+            copy_site_files(current_path, new_target)
+        else:
+            print(f"Copying file {current_path} to {destination_path}")
+            shutil.copy(current_path, destination_path)
+
+copy_site_files("static", "public")
