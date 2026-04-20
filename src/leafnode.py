274f2649ce314a9df541f7f72f75a771032252eb
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2026-04-20 13:49:58 -0500

    fix: fix image/link output

--- src/leafnode.py
+++ src/leafnode.py
@@ -7,9 +7,9 @@ class LeafNode(HTMLNode):
 
     def to_html(self):
         if self.tag == "img" and self.props is not None:
-            return f"<{self.tag} src={self.props["src"]} alt=\"{self.props["alt"]}\">"
+            return f"<{self.tag} src=\"{self.props["src"]}\" alt=\"{self.props["alt"]}\">"
         if self.tag == "a" and self.props is not None:
-            return f"<{self.tag} href={self.props["href"]}>{self.value}</{self.tag}>"
+            return f"<{self.tag} href=\"{self.props["href"]}\">{self.value}</{self.tag}>"
         elif self.tag != None:
             return f"<{self.tag}>{self.value}</{self.tag}>"
         else:
