90e97c9e0ec1f1a8517ad559c6b2fe875b35d5a0
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2026-04-20 00:18:48 -0500

    feat: implement title extraction function

---
+++ src/extract_title.py
@@ -0,0 +1,14 @@
+from src.block_to_blocktype import BlockType, block_to_block_type
+from src.markdown_to_blocks import markdown_to_blocks
+from src.markdown_to_html_node import strip_text
+
+
+def extract_title(markdown):
+    markdown_blocks = markdown_to_blocks(markdown)
+    for block in markdown_blocks:
+        block_type = block_to_block_type(block)
+        if block_type == BlockType.HEADING:
+            words = block.split(" ")
+            if words[0].count("#") == 1:
+                return strip_text(block, type = BlockType.HEADING)
+    raise Exception("No h1 title found.")
